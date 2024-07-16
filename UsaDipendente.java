7bfee16bc284cd2eb25bd519e95a4e4e492d5702
Carleoinserra <inserracarlo@gmail.com>
2024-07-16 10:23:08 +0200

    Add files via upload

--- UsaDipendente.java
+++ UsaDipendente.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UsaDipendente {
 
@@ -13,6 +15,46 @@ public class UsaDipendente {
 		//System.out.println(d1.getMansione());
 		
 		System.out.println(d1);
+		Scanner input = new Scanner(System.in);
+		
+		ArrayList <dipendente> listaD = new ArrayList<>();
+		
+		listaD.add(d1);
+		int scelta = 0;
+		
+		do {
+			System.out.println("1 per inserire un nuovo dipedente 2 per stampare 0 per terminare");
+			scelta = input.nextInt();
+			
+			if (scelta == 1) {
+				
+				System.out.println("Inserisci il nome");
+				String nome = input.next();
+				System.out.println("Inserisci la mansione");
+				String mansione = input.next();
+				System.out.println("Inserisci lo stipendioe");
+				int stipendio = input.nextInt();
+				dipendente a2 = new dipendente(nome, mansione, stipendio);
+				listaD.add(a2);}
+			
+			else if (scelta == 2) {
+				
+				for (dipendente d3: listaD) {
+					System.out.println(d3);
+				}
+				
+				
+				
+			}
+			
+			
+			
+		}
+		
+		while (scelta != 0);
+			
+			
+			
 		
 		
 	}
